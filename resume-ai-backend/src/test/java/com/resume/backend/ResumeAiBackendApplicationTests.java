326d67644ab00f285b4174ca8c73c41b6489c794
Kunal Dhanushyakar <61696534+kunalDhanushyakar@users.noreply.github.com>
2025-09-19 11:25:24 +0530

    Add files via upload
    Committing Backend of project where we have used springboot for API development and Spring-AI for connecting with Deepseek.

---
+++ resume-ai-backend/src/test/java/com/resume/backend/ResumeAiBackendApplicationTests.java
@@ -0,0 +1,23 @@
+package com.resume.backend;
+
+import com.resume.backend.service.ResumeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class ResumeAiBackendApplicationTests {
+
+	//@Autowired
+	//private ResumeService resumeService;
+
+	@Test
+	void contextLoads() throws IOException {
+
+		//resumeService.generateResumeResponse("I a Kunal Dhanushyakar with 2 years of java exp");
+
+	}
+
+}
